2d903a0e948439fe06edaf5e441cacd3be3c7309
Eina Onting <einers@outlook.com>
2020-04-15 16:43:37 -0700

    do day 2

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+const setDate = () => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+};
+
+setInterval(setDate, 1000);
